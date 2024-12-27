ace5ed6eb6bbe5903ff057c1c91512d48d14e934
melaniaspinelli <melaniaspinelli5@gmail.com>
2024-12-27 13:18:03 +0100

    modifiche al subject

--- Esame/Subject.py
+++ Esame/Subject.py
@@ -1,6 +1,18 @@
+#import sys
+#print(sys.path)
+#sys.path.append('c:\\Users\\Melania\\Documents\\GitHub\\AlphaTeam')
+#import os
+#os.chdir ('c:\\Users\\Melania\\Documents\\GitHub\\AlphaTeam')
 from BioTools import tools
 from BioTools import parserFasta
 
+def divide_into_kmer(sequence,k):
+    kmer_diz = {}
+    for i in range(0, len(sequence)-k+1,k):
+        kmer = sequence[i:i+k]
+        kmer_diz.setdefault(kmer, [])
+        kmer_diz[kmer].append(i)
+    return kmer_diz
 
 # OopCompanion:suppressRename
 class Subject:
@@ -15,20 +27,35 @@ class Subject:
             print("Il file è già nell'estensione .fasta!")
             self.subject_file = self.subject_file
 
+    #def subject_indexing(self):
+    #    diz = parserFasta.parse_fasta(self.subject_file)
+    #    kmer_diz = {}
+    #    k = 22
+    #    for header,sequence in diz.items():
+    #        kmers = tools.kmer_counting(sequence,k)
+    #        kmer_diz[header] = {}
+    #        for i,kmer in enumerate(kmers):
+    #            kmer_diz[header][kmer] = i*k
+    #    return kmer_diz
+
     def subject_indexing(self):
         diz = parserFasta.parse_fasta(self.subject_file)
-        kmer_diz = {}
-        k = 22
-        for header,sequence in diz.items():
-            kmers = tools.kmer_counting(sequence,k)
-            kmer_diz[header] = {}
-            for i,kmer in enumerate(kmers):
-                kmer_diz[header][kmer] = i*k
-        return kmer_diz
+        complete_dict = diz
+        for header, sequence in complete_dict.items():
+            complete_dict[header] = divide_into_kmer(sequence,22)
+        return complete_dict
 
+    def subject_indexing_comp_rev(self):
+        diz = parserFasta.parse_fasta(self.subject_file)
+        complete_dict = diz
+        for header, sequence in complete_dict.items():
+            complete_dict[header] = divide_into_kmer(tools.fn_comp_rev(sequence)[1],22)
+        return complete_dict
 
-sub = Subject('esempioFasta.fasta')
+sub = Subject('esempioFASTA.fa')
 sub.parse_file()
 print(sub.subject_indexing())
+print(sub.subject_indexing_comp_rev())
+
 
 
