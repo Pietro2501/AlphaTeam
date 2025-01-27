bb1b16e86d16527f42ce6b4c392814058ff192e8
Pietro2501 <p.dispaldro@studenti.uniba.it>
2025-01-27 23:12:53 +0100

    c

--- Esame/ErroriPersonalizzati.py
+++ Esame/ErroriPersonalizzati.py
@@ -15,10 +15,6 @@ class KmerError(Exception):
     def __init__(self, message="Il valore di k deve essere un intero positivo"):
         super().__init__(message)
 
-class KmerTooLong(Exception):
-    def __init__(self,message="Il valore di k è superiore alla lunghezza della sequenza"):
-        super().__init__(message)
-
 class FastaParsingError(Exception):
     def __init__(self, message="Errore nel parsing del file FASTA"):
         super().__init__(message)
@@ -27,7 +23,7 @@ class SequenceError(Exception):
     def __init__(self, message="Errore durante l'estrazione del file"):
         super().__init__(message)
 
-class EmptyDict(Exception):
+class EmptyList(Exception):
     def __init__(self, message="Il dizionario è vuoto"):
         super().__init__(message)
 
